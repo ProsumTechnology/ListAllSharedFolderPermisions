95e33ec57323e32960153fa46574bb8410220a37
Justin-DynamicD <justin.king@prosum.com>
2015-04-23 10:27:20 -0700

    Initial Sync
    Simple little PS Script that goes through all directories in a path and
    lists ACLs.

---
+++ ListAllSharedFolderPermissions.ps1
@@ -0,0 +1,50 @@
+﻿Param
+(
+	[Parameter(Mandatory=$false)][Alias('Dir')][Array]$DirList="\\ES-SSCCM-01\DSL",
+	[Parameter(Mandatory=$false)][Alias('OutFile')][String]$Output = "Results.html",
+    [Parameter(Mandatory=$false)][String]$Depth="2"
+)
+
+#Check for and load NTFSSecurity module if it's missing
+IF (!(Get-Module NTFSSecurity)) {
+    Import-Module .\NTFSSecurity\NTFSSecurity.psd1
+    }
+
+#Start with an empty array to be populated
+[array]$ChildList = $null
+
+#Add each defined URL to array with child paths
+foreach($DL in $DirList) {
+    $ChildList+=$DL
+    $ChildList+=(Get-ChildItemToDepth -Path $DL -ToDepth $Depth)
+    }
+
+#Gather all non-inherited permissions and save to HTML
+get-ntfsaccess -Path $ChildList | where {$_.IsInherited -eq $false} | Select FullName, AccountType, Account, AccessControlType, AccessRights | ConvertTo-HTML | Out-File $Output
+
+
+#This function solves the lack of depth selection in Get-ChildItem (won't be needed in PSv5)
+function Get-ChildItemToDepth {
+  param(
+    [String]$Path = $PWD,
+    [Byte]$ToDepth = 255,
+    [Byte]$CurrentDepth = 0,
+    [Switch]$DebugMode
+  )
+ 
+  $CurrentDepth++
+  if ($DebugMode) { $DebugPreference = "Continue" }
+ 
+  Get-ChildItem $Path -Directory | ForEach-Object {
+    $_.FullName
+    if ($_.PsIsContainer) {
+      if ($CurrentDepth -le $ToDepth) {
+        # Callback to this function
+        Get-ChildItemToDepth -Path $_.FullName -ToDepth $ToDepth -CurrentDepth $CurrentDepth
+      } else {
+        Write-Debug $("Skipping GCI for Folder: $($_.FullName) " +
+          "(Why: Current depth $CurrentDepth vs limit depth $ToDepth)")
+      }
+    }
+  }
+}
