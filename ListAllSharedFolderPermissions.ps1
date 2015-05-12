070be09358dcdd2b6178c7b47149f7cedb91991d
Justin-DynamicD <justin.king@prosum.com>
2015-05-11 19:56:38 -0700

    Added before and after
    now dumps the original path as well as modified path.

--- ListAllSharedFolderPermissions.ps1
+++ ListAllSharedFolderPermissions.ps1
@@ -1,9 +1,9 @@
 ﻿Param
 (
 	[Parameter(Mandatory=$false)][Alias('Dir')][Array]$DirList="\\proximus.prosum.com\DFS",
-	[Parameter(Mandatory=$false)][Alias('OutFile')][String]$Output = "Results.html",
-    [Parameter(Mandatory=$false)][Alias('OutType')][ValidateSet("CSV","HTML")][String]$OutputType = "HTML",
-    [Parameter(Mandatory=$false)][String]$Depth="255"
+	[Parameter(Mandatory=$false)][Alias('OutFile')][String]$Output = "Results.csv",
+    [Parameter(Mandatory=$false)][Alias('OutType')][ValidateSet("CSV","HTML")][String]$OutputType = "CSV",
+    [Parameter(Mandatory=$false)][String]$Depth="10"
 )
 
 #Check for and load NTFSSecurity module if it's missing
@@ -23,7 +23,7 @@ IF (!(Get-Module DFSN)) {
         Import-Module DFSN
         }
     Catch {
-        Throw "unable to load the DFSN PowerShell Module.  Please use add-remove programs to add DFS name space management tools or run from a serve rwith these tools installed"
+        Throw "unable to load the DFSN PowerShell Module.  Please use add-remove programs to add DFS name space management tools or run from a server with these tools installed"
         }
     }
 
@@ -59,12 +59,14 @@ function Get-ChildItemToDepth {
 
 #Add each defined URL to array with child paths
 write-verbose "Building full directory tree (this could take awhile) ..."
+
 foreach($DL in $DirList) {
     $ChildList+=$DL
     $ChildList+=(Get-ChildItemToDepth -Path $DL -ToDepth $Depth)
     }
 
-#Crawl Array and attempt to replace all DFS paths with valid UNC Targets
+
+#Crawl Array and attempt to identify all DFS paths with valid UNC Targets
 write-verbose "attempting to find and translate any DFS namespaces to UNC paths ... "
 [array]$DFSConvert = @()
 foreach ($DL in $ChildList) {
@@ -82,21 +84,37 @@ foreach ($DL in $ChildList) {
 #This simple sort makes sure we remap longer filename paths first and avoid breaking by mapping shorter paths first
 $DFSConvert = $DFSConvert | sort {$_Path.length}
 
-#Rebuild ChildList with new values
-write-verbose "Updating list with actual UNC paths"
-$DFSConvert | ForEach-Object {
-    $Source = [regex]::escape($_.Path)
-    $Target = $_.TargetPath
-    $ChildList = $ChildList -replace($Source,$Target)
-    } 
+#Build ChildList2 with new values
+[array]$ChildList2=$null
+write-verbose "Updating DFSNamespace links ..."
+$ChildList | ForEach-Object {
+    $Source = $_
+    $newvalue=$null
+    ForEach ($DFS in $DFSConvert) {
+        $Test=$DFS.Path
+        $Testinj = [regex]::escape($Test)
+        $TargetTest = $DFS.TargetPath
+        If ($Source -like $Test+"*") {
+            Write-verbose "need to update $_ with $targettest..."
+            $NewValue = ($_ -replace($Testinj,$TargetTest))
+            }
+        }
+    if ($newvalue -eq $null) {$ChildList2+=$Source}
+    else {$ChildList2+=$newvalue}
+    }
+
+#combine the two lists into a FinalList
+$stepsstop=$ChildList.count-1
+$range=0..$stepsstop
+foreach ($i in $range) {
+    $finallist+= @{$ChildList[$i] = $ChildList2[$i]}
+    }
 
 #Gather all non-inherited permissions and save based on defined type
 write-verbose "Generating report..."
 Switch ($OutputType) {
-    HTML {get-ntfsaccess -Path $ChildList | where {$_.IsInherited -eq $false} | Select FullName, AccountType, Account, AccessControlType, AccessRights | ConvertTo-HTML | Out-File $Output}
-    CSV {get-ntfsaccess -Path $ChildList | where {$_.IsInherited -eq $false} | Select FullName, AccountType, Account, AccessControlType, AccessRights | Export-Csv $Output}
+    HTML {get-ntfsaccess -Path $ChildList2 | where {$_.IsInherited -eq $false} | Select FullName, AccountType, Account, AccessControlType, AccessRights | ConvertTo-HTML | Out-File $Output}
+    CSV {get-ntfsaccess -Path $ChildList2 | where {$_.IsInherited -eq $false} | Select FullName, AccountType, Account, AccessControlType, AccessRights | Export-Csv $Output}
     }
 
 write-verbose "Done!"
-
-
